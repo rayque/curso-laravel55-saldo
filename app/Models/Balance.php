90895ea82649f22f0c1522653415a0915bda2a56
Rayque <rayque.oliveira@gmail.com>
2018-07-03 22:25:14 -0400

    Adicionando funcionalidades das aulas 20 a 22

--- app/Models/Balance.php
+++ app/Models/Balance.php
@@ -3,24 +3,40 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use DB;
 
 class Balance extends Model
 {
-    public $timestamps = false;
+  public $timestamps = false;
 
-    public function deposit(float $value) : Array
-    {
-      $this->amount += number_format($value, 2, '.', '');
-      $deposit = $this->save();
+  public function deposit(float $value) : Array
+  {
+    DB::beginTransaction();
 
-      if ($deposit)
+    $totalBefore = $this->amount ?? 0 ;
+    $this->amount += number_format($value, 2, '.', '');
+    $deposit = $this->save();
+
+    $historic = auth()->user()->historics()->create([
+      'type'          => 'I',
+      'amount'        => $value,
+      'total_before'  => $totalBefore,
+      'total_after'   => $this->amount,
+      'date'          => date('Ymd')
+    ]);
+
+    if ($deposit and $historic){
+      DB::commit();
       return [
         'success' => true,
         'message' => 'Sucesso ao recarregar'
       ];
+    }else{
+      DB::rollback();
       return [
         'success' => false,
         'message' => 'Falha ao recarregar'
       ];
     }
+  }
 }
--- database/migrations/2018_06_30_215050_create_historics_table.php
+++ database/migrations/2018_06_30_215050_create_historics_table.php
@@ -21,7 +21,7 @@ class CreateHistoricsTable extends Migration
             $table->double('amount', 10, 2);
             $table->double('total_before', 10, 2);
             $table->double('total_after', 10, 2);
-            $table->integer('user_id_transaction')->nullabel();
+            $table->integer('user_id_transaction')->nullable();
             $table->date('date');
             $table->timestamps();
         });
